26ee14e0c7be03e6dea4cfd78e479709798cd7d2
himanshu1818 <himanshuyadav198@gmail.com>
2021-06-03 16:22:41 +0530

    Jump animation and walk animation completed
    jump animations and walk animations have been completed and the player can now jump for twice when in air and then eventually will fall down to the idle state

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -13,11 +13,21 @@ public class PlayerMovement : MonoBehaviour
     float playerSpeed;                         // speed of the player for movement 
    
     float movementInputDirection;                                // the input that Input.GetaxisRaw will get so to decide which side player should move in x direction (i.e 1,0,-1)
-
+    private bool canJump;
     private bool isFacingRight;                  // to check if the playter is facing right 
     [SerializeField]
     private float jumpForce;
-
+    private bool isWalking;
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    LayerMask whatIsGround;
+    bool isGrounded;
+    [SerializeField]
+    private float groundCheckRadius;
+    public int amountOfJumps = 1;
+    private int amountOfLeft;
+    private bool isJumping;
 
     // Start is called before the first frame update
     void Start()
@@ -27,21 +37,26 @@ public class PlayerMovement : MonoBehaviour
         anim = GetComponent<Animator>();
         //playerSpeed = 5f;    
         isFacingRight = true;
+        amountOfLeft = amountOfJumps;
     }
-    //we have written code separately for both jumping and flipping and flipping is here in this script and jumping will be in the next one
+   
     // Update is called once per frame
     void Update()
     {
         CheckInput();
         Flip(movementInputDirection);
+        UpdateAnimations();
+        CheckIfCanJump();
 
     }
     private void FixedUpdate()
     {
         ApplyMovement();
-       
+        CheckForSurroundings();
+      //  CheckIfCanJump();
     }
 
+    //method for the flip of the player
     private void Flip(float horizontalValue)
     {
        if(horizontalValue<0 && isFacingRight || horizontalValue >0 && !isFacingRight)
@@ -51,23 +66,86 @@ public class PlayerMovement : MonoBehaviour
             scale.x *= -1;
             transform.localScale = scale;
         }
+        if (rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
     }
+    //method for checking the input given by the player
     void CheckInput()
     {
         movementInputDirection = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
+            
+        }
+    }
+    //method for the animations to play  
+    void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isJumping", isJumping);
+
+    }
+    //check for the player input if he can jump or not so to avoid double jumping
+    private void CheckIfCanJump()
+    {
+        if (isGrounded && rb.velocity.y <= 0)
+        {
+            //canJump = true;
+            amountOfLeft = amountOfJumps;
+            
+        }
+        //if the player is grounded then isjumping must be false so that player can jump and animation can be played 
+        if (isGrounded)
+        {
+            isJumping = false;
+        }
+        //if the player is not grounded then isjumping must be true as player is already jumping andmust come to idle state and animation can be played 
+        if (!isGrounded)
+        {
+            isJumping = true;
+        }
+        if (amountOfLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
+
+    }
+    // actual jump method for jumping the player
+    private void Jump()
+    {
+        if (canJump)
+        {
+          
+           rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfLeft--;
         }
     }
 
-    private void Jump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-    }
-
+    // method for applying the movement of the player 
     void ApplyMovement()
     {
         rb.velocity = new Vector2(movementInputDirection * playerSpeed, rb.velocity.y);
     }
+    // check for the player to be grounded if he's touching the ground
+    void CheckForSurroundings()
+    {
+      isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+    // gizmos to draw from the point of checkground gameobject for visual picture of the circle drawn from the point 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+   
 }
